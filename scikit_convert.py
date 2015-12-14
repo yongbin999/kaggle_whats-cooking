b581597b1413d562acb52413ba65f713af9e2d23
yongbin999 <yongbinliang@umass.edu>
2015-12-14 00:56:45 -0500

    got to fitting the model

--- scikit_convert.py
+++ scikit_convert.py
@@ -7,7 +7,7 @@ import os
 import random
 import ast
 
-def scikit_learn_format(json_data):
+def scikit_y_format(json_data):
     
     data = ast.literal_eval(json_data)
     ##print(data[0])
@@ -23,7 +23,7 @@ def scikit_learn_format(json_data):
     le_y = preprocessing.LabelEncoder()
     le_y.fit(y_terms)
     converted_y = le_y.transform(y_terms)
-
+    """
     le_x = preprocessing.LabelEncoder()
     le_x.fit(x_flat)
     converted_x = []
@@ -32,15 +32,47 @@ def scikit_learn_format(json_data):
         converted_x.append(le_x.transform(cui_ingredients))
         if (index %1000==0):
             print index
+    """
 
-    print x_terms[:2]
     print y_terms[:2]
-    print converted_x[:2]
     print converted_y[:2]
 
-    return converted_x,converted_y
+    return converted_y
 
 
+def scikit_x_vectorize(json_data):
+    from sklearn.feature_extraction import DictVectorizer
+    v = DictVectorizer(sparse=False)
+    """
+    D = [{'foo': 1, 'bar': 2}, {'foo': 3, 'baz': 1}]
+    X = v.fit_transform(D)
+    X
+    array([[ 2.,  0.,  1.],
+        [ 0.,  1.,  3.]])
+    """
+
+    data = ast.literal_eval(json_data)
+    ##print(data[0])
+    x_terms = []
+    for index,row in enumerate(data):
+        temp = {}
+        for ingre in row['ingredients']:
+            if ingre not in temp:
+                temp[ingre] =1
+
+        if (index %10000==0):
+            print index
+
+        #print temp
+        x_terms.append(temp) ## dict of each ingredient list
+    
+    #print x_terms
+    X = v.fit_transform(x_terms) ## convert dict list to ints vects matrix
+
+    print data[0]['ingredients']
+    print X[0]
+
+    return X
 
 if __name__=='__main__':
 
@@ -53,7 +85,7 @@ if __name__=='__main__':
         print 'no training files defined: python input_covert.py "___train.json___"'
         sys.exit()
 
-    data = scikit_learn_format(json_data)
+    data = scikit_x_vectorize(json_data), scikit_y_format(json_data)
 
 
 
--- scikit_random_forest.py
+++ scikit_random_forest.py
@@ -2,7 +2,9 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn import linear_model
 from sklearn import svm
 
-import input_convert
+import sklearn
+
+import scikit_convert
 
 
 
@@ -19,37 +21,64 @@ if __name__=='__main__':
         sys.exit()
 
     data = None
-    if (os.path.isfile('./outputs/scikit_converted_data_x')):
+    reloads = True
+
+    if (reloads ==False):
     	x = open('./outputs/scikit_converted_data_x','r')
-    	xlines = x.read().split("\n")
+    	xlines = x.read().split("\n")[:-1]
     	for index,each in enumerate(xlines):
-    		xlines[index] = each.split(',')
+    		xlines[index] = [float(x) for x in each.split(',')]
 
     	y = open('./outputs/scikit_converted_data_y','r')
-    	ylines = y.read().split(",")
+    	ylines = [int(x) for x in y.read().split(',')][:-1]
 
     	data = xlines,ylines
 
     else:
-    	data = input_convert.scikit_learn_format(json_data)
+    	data = scikit_convert.scikit_x_vectorize(json_data), scikit_convert.scikit_y_format(json_data)
 
 
 
 
 
     X= data[0][30000:]
-    Y= data[1][30000:]
+    y= data[1][30000:]
 
     X_test = data[0][:10000]
     y_test = data[1][:10000]
 
-    clf = linear_model.BayesianRidge()
-    clf.fit(X, Y)
+    print X[:2]
+    print y[:2]
+    
+    """
+    X = [[2, 3], [1, 1]] ## matrix
+    X = sklearn.preprocessing.normalize(X)
+    print X
+    y = [0, 1]
+    """
 
-    print clf
+	
+	clf = linear_model.BayesianRidge()
+	clf.fit(X, y)
+    from sklearn.externals import joblib
+	joblib.dump(clf, 'trained_Bayes_model.pkl') 
+	print "bayes done"
 
-    BayesianRidge(alpha_1=1e-06, alpha_2=1e-06, compute_score=False, copy_X=True,
-       fit_intercept=True, lambda_1=1e-06, lambda_2=1e-06, n_iter=300,
-       normalize=False, tol=0.001, verbose=False)
+#### SVM
 
-    print lf.predict ([['salt']])
+    clf = svm.SVC()
+    clf.fit(X, y)  
+
+    from sklearn.externals import joblib
+	joblib.dump(clf, 'trained_SVM_model.pkl') 
+
+
+    ##clf2 = pickle.loads(s)
+
+
+
+
+
+    print clf.predict([[2., 2.]])
+
+
