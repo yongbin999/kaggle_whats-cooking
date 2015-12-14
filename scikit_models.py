cbdb3f4d547a37e6123463efcb066269f4e2eab2
yongbin999 <yongbinliang@umass.edu>
2015-12-14 18:45:42 -0500

    4 out for 8 working scikit learn models

--- scikit_models.py
+++ scikit_models.py
@@ -24,26 +24,70 @@ def scikit_readdata(filename):
 	data = xlines,ylines
   	return data
 
-def scikit_GDS(x,y, X_test,y_test, prevmodel=None):
+def scikit_GDS(x,y, X_test,y_test=None, prevmodel="yes", output=False):
     from sklearn.linear_model import SGDClassifier
+    from sklearn.externals import joblib
+
     clf = SGDClassifier(loss="hinge", penalty="l2")
     ##
-    if prevmodel ==None:
+    if prevmodel !="yes":
     	clf.fit(X, y)
-    	from sklearn.externals import joblib
     	joblib.dump(clf, 'trained_GDS_model.pkl') 
     	print "done"
     else:
     	clf =joblib.load('trained_GDS_model.pkl')
 
+    if output == False:
+        predictions =  clf.predict(X_test)
+        correctcount = 0
+        totalcount = 0
+        for index, each in enumerate(predictions):
+        	if y_test[index] == each:
+        		correctcount +=1
+        	totalcount+=1
 
-    predictions =  clf.predict(X_test)
+        print str(correctcount) +" / " + str(totalcount) +" = " + str(float(correctcount)/totalcount)
+    else:
+        predictions =  clf.predict(X_test)
+        return predictions
+
+def scikit_onevsrest(X,y, X_test,y_test=None):
+    from sklearn.multiclass import OneVsRestClassifier
+    from sklearn.svm import LinearSVC
+    predictions = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X, y).predict(X_test)
     correctcount = 0
     totalcount = 0
     for index, each in enumerate(predictions):
-    	if y_test[index] == each:
-    		correctcount +=1
-    	totalcount+=1
+        if y_test[index] == each:
+            correctcount +=1
+        totalcount+=1
+
+    print str(correctcount) +" / " + str(totalcount) +" = " + str(float(correctcount)/totalcount)
+
+
+def scikit_onevsone(X,y, X_test,y_test=None):
+    from sklearn.multiclass import OneVsOneClassifier
+    from sklearn.svm import LinearSVC
+    predictions = OneVsOneClassifier(LinearSVC(random_state=0)).fit(X, y).predict(X_test)
+    correctcount = 0
+    totalcount = 0
+    for index, each in enumerate(predictions):
+        if y_test[index] == each:
+            correctcount +=1
+        totalcount+=1
+
+    print str(correctcount) +" / " + str(totalcount) +" = " + str(float(correctcount)/totalcount)
+
+def scikit_outputcode(X,y, X_test,y_test=None):
+    from sklearn.multiclass import OutputCodeClassifier
+    from sklearn.svm import LinearSVC
+    predictions = OutputCodeClassifier(LinearSVC(random_state=0),code_size=2, random_state=0).fit(X, y).predict(X_test)
+    correctcount = 0
+    totalcount = 0
+    for index, each in enumerate(predictions):
+        if y_test[index] == each:
+            correctcount +=1
+        totalcount+=1
 
     print str(correctcount) +" / " + str(totalcount) +" = " + str(float(correctcount)/totalcount)
 
@@ -61,14 +105,15 @@ if __name__=='__main__':
         print 'no training files defined: python input_covert.py "___train.json___"'
         sys.exit()
 
+
     data = scikit_convert.scikit_x_vectorize(json_data), scikit_convert.scikit_y_format(json_data)
 
 
-    X= data[0][30000:]
-    y= data[1][30000:]
+    X= data[0][:30000]
+    y= data[1][:30000]
 
-    X_test = data[0][:10000]
-    y_test = data[1][:10000]
+    X_test = data[0][30000:]
+    y_test = data[1][30000:]
 
     print X[:2]
     print y[:2]
@@ -77,15 +122,36 @@ if __name__=='__main__':
     y = [0, 1]
     """
 
+    test_data = None
+    if len(sys.argv) > 2:
+        json_test = open(sys.argv[2],'r').read()
+        test_data = scikit_convert.scikit_x_vectorize(json_test)
 
-## GDS
+
+
+## GDS = 70%
     print "running gradient descent"
     scikit_GDS(X,y,X_test,y_test,"yes")
 
 
+## one vs rest  multiclass 7515 / 9774 = 0.768876611418
+    print "one vs rest"
+    scikit_onevsrest(X,y,X_test,y_test)
+
+## output code error corrtingmulticlass 7501 / 9774 = 0.76744423982
+    print "out put code error corecting"
+    scikit_outputcode(X,y,X_test,y_test)
 
 
-##gausian nb
+
+
+
+## one vs rest  memory error
+    #print "one vs rest"
+    ##scikit_onevsone(X,y,X_test,y_test)
+
+
+##gausian nb memory error
 """
 ## naive bayes gussian
     from sklearn.naive_bayes import GaussianNB
@@ -95,7 +161,7 @@ if __name__=='__main__':
     print y_pred.predict(X_test[:1])
     print y_test[:1]
 """
-#### SVM
+#### SVM memory error
 """
     clf = svm.SVC()
     clf.fit(X, y)  
